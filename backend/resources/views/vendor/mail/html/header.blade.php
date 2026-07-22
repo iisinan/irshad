b8e9aa06023da1319f64d605f316a88f6c1506bf
ismailasinan-cmyk <ismailasinan@gmail.com>
2026-07-22 17:58:40 +0100

    Polish email templates for Irshad branding

---
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block; text-decoration: none;">
+<span style="font-size: 24px; font-weight: 800; color: #10b981; letter-spacing: -0.5px;">IRSHAD</span>
+</a>
+</td>
+</tr>
